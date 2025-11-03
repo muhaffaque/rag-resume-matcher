f8941c5e30ccbfd1b8110ddd4b11150f61eff47f
muhaffaque <muhaffaquemufu007@gmail.com>
2025-11-03 18:40:38 +0530

    config file set is completed

---
+++ src/config/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+GROQ_API_KEY = os.get_env("GROQ_API_KEY")
+EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+FAISS_DIR = "vectorstore"
